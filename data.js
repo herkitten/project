6adaf45111e700bdfbad5b7181251d4faf55f031
herkitten <160206580+herkitten@users.noreply.github.com>
2024-02-16 15:03:48 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+function extractData() {
+    const fs = require('fs');
+    fs.readFile('data.txt', (err, data) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+        const dataArray = data.toString().split("\r\n");
+        console.log(dataArray);
+
+        var dictionaryArray = []
+        for (var i = 0; i < dataArray.length; i++) {
+            var parts = dataArray[i].split(";");
+            dictionaryArray.push({ question: parts[0], answer: parts[1] });
+        }
+        console.log(dictionaryArray);
+    });
+}
